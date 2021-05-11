e716d14580aa3454ed541c2fe4f2a044af4cc588
kirill <kir.yakimov@mail.ru>
2021-05-11 10:29:09 +0300

    version 1 11/05/2021

---
+++ src/SimpleGui3C.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGui3C implements ActionListener {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SimpleGui3C gui = new SimpleGui3C();
+        gui.go();
+    }
+
+    private void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("изменить цвет");
+        button.addActionListener(this);
+
+        MyDrawPanel2 drawPanel = new MyDrawPanel2();
+
+        frame.getContentPane().add(BorderLayout.SOUTH,button);
+        frame.getContentPane().add(BorderLayout.CENTER,drawPanel);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        frame.repaint();
+
+    }
+}
